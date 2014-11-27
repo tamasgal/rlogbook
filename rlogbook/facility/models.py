39d1c419bb72c3801e019ec73c2c9439719dd2e8
Tamas Gal <himself@tamasgal.com>
2014-11-27 14:00:29 +0100

    Adds string representations to the models

--- rlogbook/facility/models.py
+++ rlogbook/facility/models.py
@@ -5,16 +5,28 @@ class Building(models.Model):
     name = models.CharField(max_length=200)
     acronym = models.CharField(max_length=10)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Room(models.Model):
     building = models.ForeignKey(Building)
     room_number = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return "{0} {1}".format(self.building.acronym, self.room_number)
 
 
 class Location(models.Model):
     room = models.ForeignKey(Room)
     description = models.CharField(max_length=200)
 
+    def __unicode__(self):
+        return "{0} - {1}".format(self.room, self.description)
+
 
 class User(models.Model):
     name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
