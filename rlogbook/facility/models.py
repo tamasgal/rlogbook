cd7f6306803e935575fd03ec6c08228995429322
Tamas Gal <himself@tamasgal.com>
2014-11-27 13:36:04 +0100

    Adds first models to facility app

--- rlogbook/facility/models.py
+++ rlogbook/facility/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Building(models.Model):
+    name = models.CharField(max_length=200)
+    acronym = models.CharField(max_length=10)
+
+
+class Room(models.Model):
+    building = models.ForeignKey(Building)
+    room_number = models.CharField(max_length=10)
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
