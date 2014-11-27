0821267cb053b14ce38398b5aa446f2749bbf8b8
Tamas Gal <himself@tamasgal.com>
2014-11-27 13:49:32 +0100

    Registers location for the admin site

--- rlogbook/facility/admin.py
+++ rlogbook/facility/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from facility.models import Building, Room, Location, User
 
-# Register your models here.
+
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(User)
+admin.site.register(Location)
+
