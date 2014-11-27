3d226e86486ff950e27cb5a40bb62584a3c1e75e
Tamas Gal <himself@tamasgal.com>
2014-11-27 14:20:33 +0100

    Installs computing app

--- rlogbook/facility/admin.py
+++ rlogbook/facility/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from facility.models import Building, Room, Location, User
-
+from computing.models import Computer, OperatingSystem
 
 admin.site.register(Building)
 admin.site.register(Room)
 admin.site.register(User)
 admin.site.register(Location)
+
+admin.site.register(Computer)
+admin.site.register(OperatingSystem)
 
