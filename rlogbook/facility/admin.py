5a13d27e6fd1086abb8f0922abc9c7c5cf4f1aae
Tamas Gal <himself@tamasgal.com>
2015-07-10 15:35:22 +0200

    Displays more information in admin view

--- rlogbook/facility/admin.py
+++ rlogbook/facility/admin.py
@@ -5,11 +5,15 @@ from facility.models import Building, Room, Location, User
 class UserAdmin(admin.ModelAdmin):
     list_display = ['name', 'arrival', 'departure']
     search_fields = ['name', 'arrival', 'departure']
+    list_filter = ['arrival', 'departure']
 
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['room_number', 'building']
+    list_filter = ['building']
 
 admin.site.register(User, UserAdmin)
 admin.site.register(Building)
-admin.site.register(Room)
+admin.site.register(Room, RoomAdmin)
 admin.site.register(Location)
 
 
